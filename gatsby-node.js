27e6e5501e2c1705268187020ce686c8faf41338
Martin Breest <24251091+mbreest@users.noreply.github.com>
2020-01-15 15:08:13 +0100

    Replaced promises with async/await.

--- gatsby-node.js
+++ gatsby-node.js
@@ -23,7 +23,7 @@ exports.createResolvers = ({ createResolvers }) => {
           }
           return "https://image.spreadshirtmedia.net/image-server/v1/mp/productTypes/" + source.contentfulid + ",width=" + args.size + ",height=" + args.size + backgroundColorParam + ".jpg";          
         },
-      },
+      },          
     },
   }
   createResolvers(resolvers)
@@ -36,9 +36,10 @@ exports.sourceNodes = async ({ actions, createNodeId, createContentDigest }) => 
   const res = await axios.get("https://designer.spreadshirt.de/api/v1/shops/1133169/productTypes?mediaType=json&fullData=true&locale=de_DE&limit=1000");
 
   res.data.productTypes.map((productType) => {
+    productType.ptid = productType.id;
     const contentDigest = crypto.createHash(`md5`).update(JSON.stringify(productType.id)).digest(`hex`);        
     const nodeMeta = {
-      id: createNodeId(`pt-${productType.id}`),
+      id: createNodeId(productType.id),
       parent: null,
       children: [],
       internal: {
@@ -54,20 +55,14 @@ exports.sourceNodes = async ({ actions, createNodeId, createContentDigest }) => 
 exports.createPages =  async ({ graphql, actions }) => {
   const { createPage } = actions;
     
-  const blog = new Promise((resolve, reject) => {
-    const blogTemplate = path.resolve('src/templates/blog.js');
-    resolve(              
-      createPage({
-        path: "/blog",
-        component: blogTemplate
-      })
-    );
-  });
-
-  const blogPosts = new Promise((resolve, reject) => {
-    const blogPostTemplate = path.resolve('src/templates/blog-post.js');
-    resolve(
-      graphql(`
+  const blogTemplate = path.resolve('src/templates/blog.js');
+  createPage({
+    path: "/blog",
+    component: blogTemplate
+  })
+  
+  const blogPostTemplate = path.resolve('src/templates/blog-post.js');
+  var result = await graphql(`
         {
             allContentfulBlogPost (limit:100) {
                 edges {
@@ -76,196 +71,127 @@ exports.createPages =  async ({ graphql, actions }) => {
                     }
                 }
             }
-        }
-          `).then((result) => {
-        if (result.errors) {
-          reject(result.errors);
-        }
-        result.data.allContentfulBlogPost.edges.forEach((edge) => {
-          createPage({
-            path: "/blog/" + edge.node.slug,
-            component: blogPostTemplate,
-            context: {
-              slug: edge.node.slug,
-            },
-          });
-        });
-        return;
-      }),
-    );
-  });
+        }`)
+  result.data.allContentfulBlogPost.edges.forEach((edge) => {
+    createPage({
+      path: "/blog/" + edge.node.slug,
+      component: blogPostTemplate,
+      context: {
+        slug: edge.node.slug,
+      },
+    });
+  });    
 
-  const authors = new Promise((resolve, reject) => {
-    const authorTemplate = path.resolve('src/templates/author.js');
-    resolve(
-      graphql(`
-        {
-            allContentfulAuthor (limit:100) {
-                edges {
-                    node {
-                        short
-                    }
-                }
-            }
-        }
-          `).then((result) => {
-        if (result.errors) {
-          reject(result.errors);
-        }
-        result.data.allContentfulAuthor.edges.forEach((edge) => {
-          createPage({
-            path: "/blog/autor/" + edge.node.short,
-            component: authorTemplate,
-            context: {
-              short: edge.node.short,
-            },
-          });
-        });
-        return;
-      }),
-    );
-  });
-
-  const blogCategories = new Promise((resolve, reject) => {
-    const blogCategoryTemplate = path.resolve('src/templates/blog-category.js');
-    resolve(
-      graphql(`
-        {
-            allContentfulBlogCategory (filter: {default: {eq: false}}, sort: {fields: index, order: ASC}, limit:100) {
-                edges {
-                    node {
-                        short
-                    }
-                }
-            }
-        }
-          `).then((result) => {
-        if (result.errors) {
-          reject(result.errors);
-        }
-        result.data.allContentfulBlogCategory.edges.forEach((edge) => {
-          createPage({
-            path: "/blog/kategorie/" + edge.node.short,
-            component: blogCategoryTemplate,
-            context: {
-              short: edge.node.short,
-            },
-          });
-        });
-        return;
-      }),
-    );
-  });
-
-  const products = new Promise((resolve, reject) => {
-    const productsTemplate = path.resolve('src/templates/products.js');
-    resolve(
-      graphql(`
-        {
-          contentfulPage(key: {eq: "products"}) {
-            short          
+  const authorTemplate = path.resolve('src/templates/author.js');
+  result = await graphql(`
+    {
+      allContentfulAuthor (limit:100) {
+        edges {
+          node {
+            short
           }
         }
-          `).then((result) => {
-          if (result.errors) {
-            reject(result.errors);
-          }
-          createPage({
-            path: result.data.contentfulPage.short,
-            component: productsTemplate
-          });
-          return;
-        }),        
-    );        
+      }
+    }`)
+  result.data.allContentfulAuthor.edges.forEach((edge) => {
+    createPage({
+      path: "/blog/autor/" + edge.node.short,
+      component: authorTemplate,
+      context: {
+        short: edge.node.short,
+      },
+    });
   });
+    
 
-  const productDetails = new Promise((resolve, reject) => {
-    const productDetailTemplate = path.resolve('src/templates/product-detail.js');
-    resolve(
-      graphql(`
-        {
-            allContentfulCatalogProduct (filter: {active: {eq: true}}, limit: 1000) {
-                edges {
-                    node {
-                        slug
-                    }
-                }
-            }
+  const blogCategoryTemplate = path.resolve('src/templates/blog-category.js');
+  result = await graphql(`
+    {
+      allContentfulBlogCategory (filter: {default: {eq: false}}, sort: {fields: index, order: ASC}, limit:100) {
+        edges {
+          node {
+            short
+          }
         }
-          `).then((result) => {
-        if (result.errors) {
-          reject(result.errors);
-        }
-        result.data.allContentfulCatalogProduct.edges.forEach((edge) => {
-          createPage({
-            path: "/detail/" + edge.node.slug,
-            component: productDetailTemplate,
-            context: {
-              slug: edge.node.slug,
-            },
-          });
-        });
-        return;
-      }),
-    );
-  });
+      }
+    }`)
+  result.data.allContentfulBlogCategory.edges.forEach((edge) => {
+    createPage({
+      path: "/blog/kategorie/" + edge.node.short,
+      component: blogCategoryTemplate,
+      context: {
+        short: edge.node.short,
+      },
+    });
+  });  
 
-  const designer = new Promise((resolve, reject) => {
-    const designerTemplate = path.resolve('src/templates/designer.js');
-    resolve(
-      graphql(`
-        {
-          contentfulPage(key: {eq: "designer"}) {
-            short          
+ const productsTemplate = path.resolve('src/templates/products.js');
+ result = await graphql(`
+    {
+      contentfulPage(key: {eq: "products"}) {
+        short          
+      }
+    }`)
+  createPage({
+    path: result.data.contentfulPage.short,
+    component: productsTemplate
+  });  
+
+ const productDetailTemplate = path.resolve('src/templates/product-detail.js');
+ result = await graphql(`
+    {
+      allContentfulCatalogProduct (filter: {active: {eq: true}}, limit: 1000) {
+        edges {
+          node {
+            slug
           }
         }
-          `).then((result) => {
-          if (result.errors) {
-            reject(result.errors);
-          }
-          createPage({
-            path: result.data.contentfulPage.short,
-            component: designerTemplate
-          });
-          return;
-        }),        
-    );    
-  });
+      }
+    }`)
+  result.data.allContentfulCatalogProduct.edges.forEach((edge) => {
+    createPage({
+      path: "/detail/" + edge.node.slug,
+      component: productDetailTemplate,
+      context: {
+        slug: edge.node.slug,
+      },
+    });
+  });  
+
+ const designerTemplate = path.resolve('src/templates/designer.js');
+ result = await graphql(`
+    {
+      contentfulPage(key: {eq: "designer"}) {
+        short          
+      }
+    }`)
+  createPage({
+    path: result.data.contentfulPage.short,
+    component: designerTemplate
+  });  
   
-  const catalogCategory = new Promise((resolve, reject) => {
-    const catalogCategoryTemplate = path.resolve('src/templates/category.js');
-    resolve(
-      graphql(`
-        {
-            allContentfulCatalogCategory (filter: {slug: {ne: "homepage"}}, limit: 1000) {
-                edges {
-                    node {
-                        slug
-                    }
-                }
-            }
-        }
-          `).then((result) => {
-        if (result.errors) {
-          reject(result.errors);
-        }        
-        result.data.allContentfulCatalogCategory.edges.forEach((edge) => {
-          let slug = "/";
-          if (edge.node.slug !== "-") {
-            slug = "/" + edge.node.slug;
+  const catalogCategoryTemplate = path.resolve('src/templates/category.js');
+  result = await graphql(`
+    {
+      allContentfulCatalogCategory (filter: {slug: {ne: "homepage"}}, limit: 1000) {
+        edges {
+          node {
+            slug
           }
-          createPage({
-            path: slug,
-            component: catalogCategoryTemplate,
-            context: {
-              slug: edge.node.slug,
-            },
-          });
-        });
-        return;
-      }),
-    );
+        }
+      }
+    }`)
+  result.data.allContentfulCatalogCategory.edges.forEach((edge) => {
+    let slug = "/";
+    if (edge.node.slug !== "-") {
+      slug = "/" + edge.node.slug;
+    }
+    createPage({
+      path: slug,
+      component: catalogCategoryTemplate,
+      context: {
+        slug: edge.node.slug,
+      },
+    });
   });
-
-  return Promise.all([blog, blogPosts, authors, blogCategories, products, productDetails, designer, catalogCategory])
 };
